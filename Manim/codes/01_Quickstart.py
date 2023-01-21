7ff2b10bba6ea15b87281cb2c6d01df99cf10685
Sagar Udasi <sagarudasi@Sagars-MacBook-Pro.local>
2023-01-22 05:11:44 +0530

    Add first tutorial on Manim - Introduction to Manim.

---
+++ Manim/codes/01_Quickstart.py
@@ -0,0 +1,16 @@
+from manim import *
+
+class FirstExample(Scene):
+    def construct(self):
+        blue_circle = Circle(color=BLUE, fill_opacity=0.5)
+        green_square = Square(color=GREEN, fill_opacity=0.8)
+        green_square.next_to(blue_circle, RIGHT)
+        self.add(blue_circle, green_square)
+
+class AnimatedExample(Scene):
+    def construct(self):
+        blue_circle = Circle(color=BLUE, fill_opacity=0.5)
+        green_square = Square(color=GREEN, fill_opacity=0.8)
+        green_square.next_to(blue_circle, RIGHT)
+        self.play(Create(blue_circle, run_time=3), FadeIn(green_square, run_time=5))
+        self.wait(2)
